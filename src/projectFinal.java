b7db03ee4bba479293aafc07136e7edb355f2809
gabigens <gabi_nalves@hotmail.com>
2022-09-05 22:03:02 -0300

    codingTankB3

---
+++ src/projectFinal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class projectFinal {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá, seja bem-vindo ao meu conversor de temperaturas.");
+        System.out.println("Você poderá escolher duas unidades de temperatura: a que você já tem e a unidade que deseja descobrir. Vamos começar?");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Antes de mais nada, quantos valores deseja converter?");
+        int numberoftemps = input.nextInt();
+
+
+    }
+}
+// * conseguir transformar temperatura em Celsius, Kelvin e Fahrenheit a partir de qualquer uma dessas unidades para qualquer uma dessas unidades.
+// * imprimir cada temperatura passada, e a unidade escolhida, e também a temperatura resultante, transformada, com sua respectiva unidade.
+// * calcular a média das temperaturas iniciais e transformadas.
+
+//public static double CelsiusToFarenheit (double celsius, double farenheit)
+
